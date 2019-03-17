7f6b2bf717f0016d82d2e2d4ad70f3e3657839c8
dsoccer1980 <dsoccer1980@gmail.com>
2019-03-17 21:09:25 +0200

    add Author, Genre, Book

---
+++ src/main/java/ru/dsoccer1980/domain/Book.java
@@ -0,0 +1,31 @@
+package ru.dsoccer1980.domain;
+
+public class Book {
+    private int id;
+    private String name;
+    private Author author;
+    private Genre genre;
+
+    public Book(int id, String name, Author author, Genre genre) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+}
---
+++ src/main/java/ru/dsoccer1980/domain/Genre.java
@@ -0,0 +1,19 @@
+package ru.dsoccer1980.domain;
+
+public class Genre {
+    private int id;
+    private String name;
+
+    public Genre(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
