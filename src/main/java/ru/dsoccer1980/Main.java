aba28f760913ecd416fad8f637cffc9d4082139b
denis <dsoccer1980@gmail.com>
2019-03-19 21:25:48 +0000

    Add BookCommands

--- src/main/java/ru/dsoccer1980/Main.java
+++ src/main/java/ru/dsoccer1980/Main.java
@@ -2,27 +2,13 @@ package ru.dsoccer1980;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import ru.dsoccer1980.dao.AuthorDao;
-import ru.dsoccer1980.dao.BookDao;
-import ru.dsoccer1980.dao.GenreDao;
-import ru.dsoccer1980.domain.Book;
 
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(Main.class);
+        SpringApplication.run(Main.class);
 
-        AuthorDao authorDao = applicationContext.getBean(AuthorDao.class);
-        System.out.println(authorDao.getAll());
-        GenreDao genreDao = applicationContext.getBean(GenreDao.class);
-        System.out.println(genreDao.getAll());
-
-        BookDao bookDao = applicationContext.getBean(BookDao.class);
-        System.out.println(bookDao.getAll());
-        bookDao.insert(new Book(2, "New", authorDao.getById(1), genreDao.getById(1)));
-        System.out.println(bookDao.getAll());
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,17 @@
+DELETE FROM Book;
+DELETE FROM Author;
+DELETE FROM Genre;
+
 INSERT INTO Author(id, name) VALUES (1, 'Стругацкий');
+INSERT INTO Author(id, name) VALUES (2, 'Уэллс');
+INSERT INTO Author(id, name) VALUES (3, 'Пушкин');
 
-INSERT INTO Genre(id, name) VALUES (1, 'Фантастика');
+INSERT INTO Genre(id, name) VALUES (10, 'Фантастика');
+INSERT INTO Genre(id, name) VALUES (11, 'Классика');
 
-INSERT INTO Book(id, name, author_id, genre_id)   VALUES (1, 'Трудно быть Богом', 1, 1);
+INSERT INTO Book(id, name, author_id, genre_id)   VALUES (100, 'Трудно быть Богом', 1, 10);
+INSERT INTO Book(id, name, author_id, genre_id)   VALUES (101, 'Машина времени', 2, 10);
+INSERT INTO Book(id, name, author_id, genre_id)   VALUES (102, 'Онегин', 3, 11);
+
+
+
